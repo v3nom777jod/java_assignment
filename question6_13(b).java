268eaae2ed9534a29458cd4624381f3d0cd1930b
Anurag777 <142607062+v3nom777jod@users.noreply.github.com>
2024-04-22 09:41:56 +0530

    question6_13(b).java

---
+++ question6_13(b).java
@@ -0,0 +1,18 @@
+//using else if statement
+public class question6_13(b) {
+        public static void main(String[] args) {
+            int x = 5;
+            int y;
+    
+            if (x > 0) {
+                y = 1;
+            } else if (x < 0) {
+                y = -1;
+            } else {
+                y = 0;
+            }
+    
+            System.out.println("y = " + y);
+        }
+        
+}
