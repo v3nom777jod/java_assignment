81b968c68154dbbbc0eacbfaf8f445d257280653
Anurag777 <142607062+v3nom777jod@users.noreply.github.com>
2024-04-22 10:04:16 +0530

    question7_10_c.java

---
+++ question7_10_c.java
@@ -0,0 +1,14 @@
+
+public class question7_10_c {
+        public static void main(String[] args) {
+            for (int i=1;i<=5;i++) {
+                for (int j=5;j>i;j--) {
+                    System.out.print(" ");
+                }
+                for (int k=1;k<=i;k++) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+            }
+        }  
+}
