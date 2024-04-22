38760356ee6e0287e4e3403db494e7b7aa2185bd
Anurag777 <142607062+v3nom777jod@users.noreply.github.com>
2024-04-22 10:03:42 +0530

    question7_10_b.java

---
+++ question7_10_b.java
@@ -0,0 +1,12 @@
+
+public class question7_10_b {
+        public static void main(String[] args) {
+            for (int i=5;i>=1;i--) {
+                for (int j=1;j<=i;j++) {
+                    System.out.print("$ ");
+                }
+                System.out.println();
+            }
+        }
+     
+}
