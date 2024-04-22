a11bff691303228c3e8ac03075cc5f9088bf521e
Anurag777 <142607062+v3nom777jod@users.noreply.github.com>
2024-04-22 09:44:01 +0530

    question6_13(c).java

---
+++ question6_13(c).java
@@ -0,0 +1,9 @@
+//using conditional operator
+public class question6_13(c) {
+        public static void main(String[] args) {
+            int x=5;
+            int y=(x>0)?1:((x<0)?-1:0);
+    
+            System.out.println("y = " + y);
+        } 
+}
