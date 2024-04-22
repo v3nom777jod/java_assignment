140402e56e864ec8613200430cbf25924a47613e
Anurag777 <142607062+v3nom777jod@users.noreply.github.com>
2024-04-22 09:41:04 +0530

    question6_13(a).java

---
+++ question6_13(a).java
@@ -0,0 +1,18 @@
+//using Nested if statement
+public class question6_13(a){
+        public static void main(String[] args) {
+            int x = 5;
+            int y;
+    
+            if (x > 0) {
+                y = 1;
+            } else if (x < 0) {
+                y = -1;
+            } else {
+                y = 0;
+            }
+    
+            System.out.println("y = " + y);
+        }
+    
+}
